9a42333300df7dafc1628778572b309081cb913f
sangsakaa <zainuri717@gmail.com>
2022-09-22 07:44:22 +0700

    logo

--- resources/views/siswa/transkip.blade.php
+++ resources/views/siswa/transkip.blade.php
@@ -42,7 +42,7 @@
                     </div>
                     <div id="div1">
                         <p class=" capitalize text-center mt-2 mb-2 text-2xl ">kartu hasil tadris</p>
-                        <div class=" grid sm:grid-cols-4 grid-cols-2">
+                        <div class=" grid sm:grid-cols-4 grid-cols-4">
                             <div>Nomor Induk Siswa</div>
                             <div class=" text-sm">: {{$tittle->nis}} </div>
 
