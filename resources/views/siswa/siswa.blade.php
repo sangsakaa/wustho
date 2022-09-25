b166702a4ccd6e4f4ad5cb60b6aad7d87ec54c88
sangsakaa <zainuri717@gmail.com>
2022-09-25 20:20:09 +0700

    biodata

--- resources/views/siswa/siswa.blade.php
+++ resources/views/siswa/siswa.blade.php
@@ -103,8 +103,8 @@
                                     ?>
                                 </td>
 
-                                @role('admin')
                                 <td class=" text-sm flex justify-center py-1  gap-1">
+                                    @role('admin')
                                     <form action="/siswa/{{$peserta->id}}" method="post">
                                         @csrf
                                         @method('delete')
@@ -112,6 +112,7 @@
                                                 <path stroke-linecap="round" stroke-linejoin="round" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" />
                                             </svg></button>
                                     </form>
+                                    @endrole
                                     <a href="/siswa/{{$peserta->id}}/edit" class=" bg-yellow-500 rounded p-1 flex ">
                                         <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                                             <path stroke-linecap="round" stroke-linejoin="round" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" />
@@ -120,7 +121,7 @@
                                         Detail
                                     </a>
                                 </td>
-                                @endrole
+
                             </tr>
                             @endforeach
                             @else
