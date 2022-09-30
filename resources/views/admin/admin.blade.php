4bbb18bd38a657c78b6f8369af4b2a856bcc21f0
sangsakaa <zainuri717@gmail.com>
2022-09-30 16:53:36 +0700

    user manajement

--- resources/views/admin/admin.blade.php
+++ resources/views/admin/admin.blade.php
@@ -5,7 +5,23 @@
       {{ __('Dashboard Manajemen User') }}
     </h2>
   </x-slot>
-  <div class=" grid grid-cols-1 sm:grid-cols-1 gap-2 px-2 py-2">
+  <div class=" grid grid-cols-1 sm:grid-cols-1 gap-2 px-2 mt-4">
+    <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+      <div class=" bg-white border-b border-gray-200">
+        <div class=" p-4 grid grid-cols-1">
+          <span class=" text-sky-500"> User Management Role</span>
+          <div class=" flex grid-cols-1 gap-1">
+            <a href="#" class=" bg-sky-400 py-1 px-4 rounded-md text-white hover:bg-purple-500">AddUsers</a>
+            <a href="#" class=" bg-sky-400 py-1 px-4 rounded-md text-white hover:bg-purple-500">Manajemen Role User</a>
+            <a href="#" class=" bg-sky-400 py-1 px-4 rounded-md text-white hover:bg-purple-500">Has Role</a>
+          </div>
+
+
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class=" grid grid-cols-1 sm:grid-cols-1 gap-2 px-2 mt-4">
     <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
       <div class=" bg-white border-b border-gray-200">
         <div class=" p-4 grid grid-cols-1">
