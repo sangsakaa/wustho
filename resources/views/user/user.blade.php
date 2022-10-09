89eb052ba9a7c8cf948827a63f243f0f65abe9aa
sangsakaa <zainuri717@gmail.com>
2022-10-10 04:48:48 +0700

    header

--- resources/views/user/user.blade.php
+++ resources/views/user/user.blade.php
@@ -5,22 +5,21 @@
             {{ __('Dashboard') }}
         </h2>
     </x-slot>
-    <div class=" flex grid-cols-2 p-4 gap-2 ">
-        <div class="  rounded bg-white ">
-            <img src={{ asset("asset/images/logo_kop.jpeg") }} alt="" width="300px" class=" p-2">
+    <div class=" p-2 sm:p-4 sm:flex grid sm:grid-cols-1  grid-cols-1 gap-2 ">
+        <div class=" text-center  sm:text-center   rounded bg-white ">
+            <center>
+                <img src={{ asset("asset/images/logo_kop.jpeg") }} alt="" width="300" class=" h-300 p-2">
+            </center>
         </div>
-        <div class=" p-4   grid-cols-2 w-full bg-white ">
-            <div class=" px-4 border">Nomor Induk siswa</div>
-            <div class=" px-4 border"> {{$siswa->nis}}</div>
-            <div class=" px-4 border">Nama Lengkap </div>
-            <div class=" px-4 border"> {{$siswa->nama_siswa}}</div>
-            <div class=" px-4 border">Tempat Lahir</div>
-            <div class=" px-4 border"> {{$siswa->tempat_lahir}}</div>
-            <div class=" px-4 border">Tanggal Lahir</div>
-            <div class=" px-4 border"> {{$siswa->tanggal_lahir}}</div>
-
-
-
+        <div class=" p-4    w-full    grid-cols-2 sm:grid-cols-2  bg-white ">
+            <div class=" px-1 border">Nomor Induk siswa</div>
+            <div class=" px-1 border"> {{$siswa->nis}}</div>
+            <div class=" px-1 border">Nama Lengkap </div>
+            <div class=" px-1 border"> {{$siswa->nama_siswa}}</div>
+            <div class=" px-1 border">Tempat Lahir</div>
+            <div class=" px-1 border"> {{$siswa->tempat_lahir}}</div>
+            <div class=" px-1 border">Tanggal Lahir</div>
+            <div class=" px-1 border"> {{$siswa->tanggal_lahir}}</div>
         </div>
     </div>
 
