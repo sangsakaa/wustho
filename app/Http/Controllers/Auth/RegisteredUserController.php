d15f011f857a5e458297a337cecfee53ca1a04cf
sangsakaa <zainuri717@gmail.com>
2022-10-11 18:17:36 +0700

    dashboard

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -124,9 +124,17 @@ class RegisteredUserController extends Controller
         }
 
         event(new Registered($user));
-        Auth::login($user);
+        // Auth::login($user);
+        if (Auth::login($user)) {
+            return redirect(RouteServiceProvider::USER);
+        } else {
+            return redirect(RouteServiceProvider::HOME);
+        }
 
-        return redirect(RouteServiceProvider::HOME);
+
+        
+        // return redirect(RouteServiceProvider::HOME);
+        
       
         
     }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -415,11 +415,34 @@
             </div>
             <div class="mt-8 bg-white dark:bg-gray-800 overflow-hidden shadow sm:rounded-lg">
                 <div class="grid grid-cols-1 md:grid-cols-1">
+                    <div class="p-6 text-center">
+                        @if(Route::has('login'))
+                        @auth
+                        Selemat Anda Sudah Login
+                        @else
+                        <a href="{{ route('login') }}" class="text-sm text-gray-700  dark:text-white ">Log in</a>
+                        @endif
+                        @endauth
+                    </div>
                     @role('super admin')
                     <div class="p-6 text-center">
                         @if(Route::has('login'))
                         @auth
                         <a href="{{ url('/dashboard') }}" class="text-md text-gray-700 dark:text-gray-500 ">Dashboard</a>
+                        @else
+                        <a href="{{ route('login') }}" class="text-sm text-gray-700  dark:text-white ">Log in</a>
+                        @endif
+                        @endauth
+                        <div class="ml-4 text-center text-sm text-gray-500 sm:text-right sm:ml-0">
+                            Laravel v {{ Illuminate\Foundation\Application::VERSION }} (PHP v {{ PHP_VERSION }})
+                        </div>
+                    </div>
+                    @endrole
+                    @role('siswa')
+                    <div class="p-6 text-center">
+                        @if(Route::has('login'))
+                        @auth
+                        <a href="{{ url('/userdashboard') }}" class="text-md text-gray-700 dark:text-gray-500 ">User Dashboard</a>
                         @else
                         <a href="{{ route('login') }}" class="text-sm text-gray-700  dark:text-white ">Log in</a>
                         @endif
