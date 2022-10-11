f558cb74127a0a51fb428b065e3578fa35c97785
sangsakaa <zainuri717@gmail.com>
2022-10-11 10:24:51 +0700

    role pengurus

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -11,7 +11,6 @@
                 </div>
                 <!-- Navigation Links -->
                 @role('siswa')
-
                 <div class="hidden space-x-8 sm:-my-px sm:ml-5 sm:flex">
                     <x-nav-link :href="route('user')" :active="request()->routeIs('user')">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
@@ -44,6 +43,13 @@
                             <path stroke-linecap="round" stroke-linejoin="round" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" />
                         </svg>
                         {{ __('Asrama') }}
+                    </x-nav-link>
+                </div>
+                <div class="hidden text-xs space-x-8 sm:-my-px sm:ml-5 sm:flex">
+                    <x-nav-link :href="route('siswa')" :active="request()->routeIs('siswa')">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M5.121 17.804A13.937 13.937 0 0112 16c2.5 0 4.847.655 6.879 1.804M15 10a3 3 0 11-6 0 3 3 0 016 0zm6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
+                        </svg> {{ __('Siswa') }}
                     </x-nav-link>
                 </div>
                 @endrole
