a8b46a9f1481a30dd19576f3068b7faf521bfbd5
sangsakaa <zainuri717@gmail.com>
2022-10-12 07:41:00 +0700

    ipk

--- resources/views/user/userdashboard.blade.php
+++ resources/views/user/userdashboard.blade.php
@@ -16,7 +16,7 @@
                         </div>
                         <div class=" bg-sky-400 p-6 text-center text-white rounded-md">
 
-                            <span>IPK : {{number_format($b),2,2,}}</span>
+                            <span>IPK : {{(number_format($b,2,','))}}</span>
                         </div>
                     </div>
 
