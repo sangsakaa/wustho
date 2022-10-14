ecee0e34c7d6a2141384fc8dc129fbe485e23f1c
sangsakaa <zainuri717@gmail.com>
2022-10-15 00:19:42 +0700

    role

--- resources/views/components/sidebar/content.blade.php
+++ resources/views/components/sidebar/content.blade.php
@@ -1,10 +1,15 @@
 <x-perfect-scrollbar as="nav" aria-label="main" class="flex flex-col flex-1 gap-4 px-3">
 
-    <x-sidebar.link title="Dashboard" href="dashboard" :isActive="request()->routeIs('dashboard')">
+    @role('super admin')
+    <x-sidebar.link title="Dashboard" href="userdashboard" :isActive="request()->routeIs('userdashboard')">
         <x-slot name="icon">
             <x-icons.dashboard class="flex-shrink-0 w-6 h-6" aria-hidden="true" />
         </x-slot>
     </x-sidebar.link>
+    @endrole
+    @role('siswa')
+
+    @endrole
 
     <!-- <x-sidebar.dropdown title="Buttons" :active="Str::startsWith(request()->route()->uri(), 'buttons')">
         <x-slot name="icon">
