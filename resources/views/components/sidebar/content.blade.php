99f4f13bb52edd539d268c4fab0f02da906a98c1
sangsakaa <zainuri717@gmail.com>
2022-10-15 09:07:21 +0700

    raport

--- resources/views/components/sidebar/content.blade.php
+++ resources/views/components/sidebar/content.blade.php
@@ -37,6 +37,11 @@
             <x-icons.usercircle class="flex-shrink-0 w-6 h-6" aria-hidden="true" />
         </x-slot>
     </x-sidebar.link>
+    <x-sidebar.link title=" Data Guru" href="/siswa" :isActive="request()->routeIs('guru')">
+        <x-slot name="icon">
+            <x-icons.usercircle class="flex-shrink-0 w-6 h-6" aria-hidden="true" />
+        </x-slot>
+    </x-sidebar.link>
 
     <x-sidebar.link title=" Data Kelas" href="/kelas_mi" :isActive="request()->routeIs('kelas_mi')">
         <x-slot name="icon">
