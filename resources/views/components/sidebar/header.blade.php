67e9e9a9fc52ad11d81c781f5e5d5e19ebe3a4b6
sangsakaa <zainuri717@gmail.com>
2022-10-16 18:42:00 +0700

    presensi

--- resources/views/components/sidebar/header.blade.php
+++ resources/views/components/sidebar/header.blade.php
@@ -6,13 +6,18 @@
         <span class="sr-only text-black">S M E D I</span>
     </a>
     @endrole
+    @role(' pengurus')
+    <a href="{{ route('dashboard') }}" class="inline-flex items-center gap-2">
+        <x-application-logo aria-hidden="true" class="w-10 h-auto" />
+        <span class="sr-only text-black">S M E D I</span>
+    </a>
+    @endrole
     @role('siswa')
     <a href="{{ route('userdashboard') }}" class="inline-flex items-center gap-2">
         <x-application-logo aria-hidden="true" class="w-10 h-auto" />
         <span class="sr-only text-black">S M E D I</span>
     </a>
     @endrole
-
     <!-- Toggle button -->
     <x-button type="button" iconOnly srText="Toggle sidebar" variant="secondary" x-show="isSidebarOpen || isSidebarHovered" @click="isSidebarOpen = !isSidebarOpen">
         <x-icons.menu-fold-right x-show="!isSidebarOpen" aria-hidden="true" class="hidden w-6 h-6 lg:block" />
