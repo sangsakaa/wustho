418458512666dd80f06ec13bb282abe66ccde0fc
sangsakaa <zainuri717@gmail.com>
2022-10-16 18:41:04 +0700

    presensi

--- resources/views/components/sidebar/content.blade.php
+++ resources/views/components/sidebar/content.blade.php
@@ -1,13 +1,13 @@
 <x-perfect-scrollbar as="nav" aria-label="main" class="flex flex-col flex-1 gap-4 px-3">
     @role('super admin')
-    <x-sidebar.link title="Dashboard" href="#" :isActive="request()->routeIs('dashboard')">
+    <x-sidebar.link title="Dashboard" href="dashboard" :isActive="request()->routeIs('dashboard')">
         <x-slot name="icon">
             <x-icons.dashboard class="flex-shrink-0 w-6 h-6" aria-hidden="true" />
         </x-slot>
     </x-sidebar.link>
     @endrole
     @role('pengurus')
-    <x-sidebar.link title="Dashboard" href="#" :isActive="request()->routeIs('dashboard')">
+    <x-sidebar.link title="Dashboard" href="dashboard" :isActive="request()->routeIs('dashboard')">
         <x-slot name="icon">
             <x-icons.dashboard class="flex-shrink-0 w-6 h-6" aria-hidden="true" />
         </x-slot>
