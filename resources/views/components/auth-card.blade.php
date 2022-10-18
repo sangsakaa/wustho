ba6a8cfad60ac7887a7e42a5427a0957f32469e2
sangsakaa <zainuri717@gmail.com>
2022-10-19 00:16:57 +0700

    design

--- resources/views/components/auth-card.blade.php
+++ resources/views/components/auth-card.blade.php
@@ -1,11 +1,11 @@
 <main class="flex flex-col items-center flex-1 px-4 pt-6 text-center capitalize sm:justify-center">
-    @role('super admin')
+
     <div>
         <a href="#3">
             <img {{ $attributes }} src="{{asset('asset/images/logo.png')}}" width="100" alt="">
         </a>
     </div>
-    @endrole
+
     <div class="w-full px-6 py-4 my-6 overflow-hidden bg-white rounded-md shadow-md sm:max-w-md dark:bg-dark-eval-1">
         {{ $slot }}
     </div>
