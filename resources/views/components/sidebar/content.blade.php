7668782d417cd1ca3c8eaacda39a600c0532c92b
sangsakaa <zainuri717@gmail.com>
2022-10-22 20:33:47 +0700

    dash

--- resources/views/components/sidebar/content.blade.php
+++ resources/views/components/sidebar/content.blade.php
@@ -1,20 +1,20 @@
 <x-perfect-scrollbar as="nav" aria-label="main" class="flex flex-col flex-1 gap-4 px-3">
     @role('super admin')
-    <x-sidebar.link title="Dashboard" href="dashboard" :isActive="request()->routeIs('dashboard')">
+    <x-sidebar.link title="Dashboard" href="/dashboard" :isActive="request()->routeIs('dashboard')">
         <x-slot name="icon">
             <x-icons.dashboard class="flex-shrink-0 w-6 h-6" aria-hidden="true" />
         </x-slot>
     </x-sidebar.link>
     @endrole
     @role('pengurus')
-    <x-sidebar.link title="Dashboard" href="dashboard" :isActive="request()->routeIs('dashboard')">
+    <x-sidebar.link title="Dashboard" href="/dashboard" :isActive="request()->routeIs('dashboard')">
         <x-slot name="icon">
             <x-icons.dashboard class="flex-shrink-0 w-6 h-6" aria-hidden="true" />
         </x-slot>
     </x-sidebar.link>
     @endrole
     @role('siswa')
-    <x-sidebar.link title="Dashboard" href="userdashboard" :isActive="request()->routeIs('userdashboard')">
+    <x-sidebar.link title="Dashboard" href="/userdashboard" :isActive="request()->routeIs('userdashboard')">
         <x-slot name="icon">
             <x-icons.dashboard class="flex-shrink-0 w-6 h-6" aria-hidden="true" />
         </x-slot>
