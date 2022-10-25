8d6708fd5adb1b1d5f0c7b94fa8bb1de00ae4387
sangsakaa <zainuri717@gmail.com>
2022-10-25 21:31:46 +0700

    print

--- resources/views/asrama/pesertaasrama.blade.php
+++ resources/views/asrama/pesertaasrama.blade.php
@@ -4,6 +4,16 @@
             Dashboard Peserta Asrama {{$tittle->nama_asrama}}
         </h2>
     </x-slot>
+    <script>
+        function printContent(el) {
+            var fullbody = document.body.innerHTML;
+            var printContent = document.getElementById(el).innerHTML;
+            document.body.innerHTML = printContent;
+            window.print();
+            document.body.innerHTML = fullbody;
+        }
+    </script>
+
 
     <div class="px-4 py-2">
 
@@ -51,77 +61,83 @@
                                 </button>
                             </div>
 
-
-
+                        </a>
+                        <button class="flex text-white rounded-md  bg-green-800 px-2 py-1 " onclick="printContent('div1')">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M6.72 13.829c-.24.03-.48.062-.72.096m.72-.096a42.415 42.415 0 0110.56 0m-10.56 0L6.34 18m10.94-4.171c.24.03.48.062.72.096m-.72-.096L17.66 18m0 0l.229 2.523a1.125 1.125 0 01-1.12 1.227H7.231c-.662 0-1.18-.568-1.12-1.227L6.34 18m11.318 0h1.091A2.25 2.25 0 0021 15.75V9.456c0-1.081-.768-2.015-1.837-2.175a48.055 48.055 0 00-1.913-.247M6.34 18H5.25A2.25 2.25 0 013 15.75V9.456c0-1.081.768-2.015 1.837-2.175a48.041 48.041 0 011.913-.247m10.5 0a48.536 48.536 0 00-10.5 0m10.5 0V3.375c0-.621-.504-1.125-1.125-1.125h-8.25c-.621 0-1.125.504-1.125 1.125v3.659M18 10.5h.008v.008H18V10.5zm-3 0h.008v.008H15V10.5z" />
+                            </svg>
+                            Anggota Asrama
+                        </button>
                     </div>
-                    </a>
 
-                    <Table class=" w-full">
-                        <thead>
-                            <tr class=" border ">
-                                <th class=" border py-1">#</th>
-                                <th class=" border text-center px-2">NIS</th>
-                                <th class=" border text-left px-2"> Daftar Peserta</th>
-                                <th class=" border text-center w-1">JK</th>
-                                <th class=" border text-center px-2"> Asrama</th>
-                                <th class=" border text-center">Kota Asal</th>
-                                @role('super admin')
-                                <th class=" border text-center">Aksi</th>
-                                @endrole
-                            </tr>
-                        </thead>
-                        <tbody>
-                            @foreach($datapeserta as $siswa)
-                            <tr class="  hover:bg-gray-100">
-                                <td class=" border px-2 text-center">
-                                    {{$loop->iteration}}
-                                </td>
-                                <td class=" border px-2 w-20">
-                                    @if($siswa->nis == $siswa->nis)
-                                    <span class=" text-red-500">{{$siswa->nis}}</span>
-                                    @else($siswa->nis != $siswa->nis)
-                                    <span class=" text-yellow-500">{{$siswa->nis}}</span>
-                                    @endif
-                                </td>
-                                <td class=" border px-2">
-                                    {{$siswa->nama_siswa}}
-                                </td>
-                                <td class=" border px-2">
-                                    {{$siswa->jenis_kelamin}}
-                                </td>
-                                <td class=" border px-2 text-center">
-                                    {{$siswa->nama_asrama}}
-                                </td>
-                                <td class=" border px-2">
-                                    {{$siswa->kota_asal}}
-                                </td>
-                                @role('super admin')
-                                <td class=" text-sm flex justify-center py-1  gap-1 border">
-                                    <form action="/pesertaasrama/{{$siswa->id}}" method="post">
-                                        @csrf
-                                        @method('delete')
-                                        <button class=" bg-red-500 text-white p-1 rounded-md flex"><svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-                                                <path stroke-linecap="round" stroke-linejoin="round" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" />
-                                            </svg></button>
-                                    </form>
-                                    <a href="/pesertaasrama/{{$siswa->id}}/edit" class=" bg-yellow-500 rounded p-1 flex ">
-                                        <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-                                            <path stroke-linecap="round" stroke-linejoin="round" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" />
-                                        </svg></a>
+                    <div id="div1">
+                        <Table class=" mt-1 w-full">
+                            <thead>
+                                <tr class=" border ">
+                                    <th class=" border py-1">#</th>
+                                    <th class=" border text-center px-2">NIS</th>
+                                    <th class=" border text-left px-2"> Daftar Peserta</th>
+                                    <th class=" border text-center w-1">JK</th>
+                                    <th class=" border text-center px-2"> Asrama</th>
+                                    <th class=" border text-center">Kota Asal</th>
+                                    @role('super admin')
+                                    <th class=" border text-center">Aksi</th>
+                                    @endrole
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach($datapeserta as $siswa)
+                                <tr class="  hover:bg-gray-100">
+                                    <td class=" border px-2 text-center">
+                                        {{$loop->iteration}}
+                                    </td>
+                                    <td class=" border px-2 w-20">
+                                        @if($siswa->nis == $siswa->nis)
+                                        <span class=" text-red-500">{{$siswa->nis}}</span>
+                                        @else($siswa->nis != $siswa->nis)
+                                        <span class=" text-yellow-500">{{$siswa->nis}}</span>
+                                        @endif
+                                    </td>
+                                    <td class=" border px-2">
+                                        {{$siswa->nama_siswa}}
+                                    </td>
+                                    <td class=" border px-2">
+                                        {{$siswa->jenis_kelamin}}
+                                    </td>
+                                    <td class=" border px-2 text-center">
+                                        {{$siswa->nama_asrama}}
+                                    </td>
+                                    <td class=" border px-2">
+                                        {{$siswa->kota_asal}}
+                                    </td>
+                                    @role('super admin')
+                                    <td class=" text-sm flex justify-center py-1  gap-1 border">
+                                        <form action="/pesertaasrama/{{$siswa->id}}" method="post">
+                                            @csrf
+                                            @method('delete')
+                                            <button class=" bg-red-500 text-white p-1 rounded-md flex"><svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                                                    <path stroke-linecap="round" stroke-linejoin="round" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" />
+                                                </svg></button>
+                                        </form>
+                                        <a href="/pesertaasrama/{{$siswa->id}}/edit" class=" bg-yellow-500 rounded p-1 flex ">
+                                            <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                                                <path stroke-linecap="round" stroke-linejoin="round" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" />
+                                            </svg></a>
 
 
-                                </td>
-                                @endrole
-                            </tr>
-                            @endforeach
-                            <tr>
-                                <td colspan="6">
+                                    </td>
+                                    @endrole
+                                </tr>
+                                @endforeach
+                                <tr>
+                                    <td colspan="6">
 
-                                </td>
-                            </tr>
+                                    </td>
+                                </tr>
 
-                        </tbody>
-                    </Table>
+                            </tbody>
+                        </Table>
+                    </div>
                 </div>
             </div>
         </div>
