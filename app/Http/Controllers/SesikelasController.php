5e5b86c6b09c4058489b86922fd76a449cf953e6
sangsakaa <zainuri717@gmail.com>
2022-10-30 18:01:32 +0700

    tgl

--- app/Http/Controllers/SesikelasController.php
+++ app/Http/Controllers/SesikelasController.php
@@ -25,6 +25,7 @@ class SesikelasController
             ->join('periode', 'periode.id', 'kelasmi.periode_id')
             ->join('semester', 'semester.id', 'periode.semester_id')
             ->select('sesikelas.*', 'kelasmi.nama_kelas', 'periode.periode', 'semester.ket_semester')
+            ->orderByDesc('sesikelas.tgl')
             ->where('kelasmi.periode_id', session('periode_id'));
 
         if (request('cari')) {
