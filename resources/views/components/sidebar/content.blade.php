87c681bcb2b740c9f946828e24d506bce1428a5c
sangsakaa <zainuri717@gmail.com>
2022-12-05 06:16:11 +0700

    absen

--- resources/views/components/sidebar/content.blade.php
+++ resources/views/components/sidebar/content.blade.php
@@ -126,6 +126,11 @@
             <x-icons.home class="flex-shrink-0 w-6 h-6" aria-hidden="true" />
         </x-slot>
     </x-sidebar.link>
+    <x-sidebar.link title="Rekap Harian Jama'ah" href="/rekap-harian" :isActive="request()->routeIs('rekap-harian')">
+        <x-slot name="icon">
+            <x-icons.home class="flex-shrink-0 w-6 h-6" aria-hidden="true" />
+        </x-slot>
+    </x-sidebar.link>
     <x-sidebar.link title="Kegiatan" href="/kegiatan" :isActive="request()->routeIs('kegiatan')">
         <x-slot name="icon">
             <x-icons.home class="flex-shrink-0 w-6 h-6" aria-hidden="true" />
