ef4d682b893d5ef3b4d4fbe53b0e05cd22af56a0
sangsakaa <zainuri717@gmail.com>
2022-12-13 13:04:24 +0700

    asrama

--- resources/views/asrama/asrama.blade.php
+++ resources/views/asrama/asrama.blade.php
@@ -100,7 +100,7 @@
                                     <th class=" text-center border">{{$loop->iteration}}</th>
                                     <td class=" text-center border"> {{$buah->nama_asrama}}</td>
                                     <td class=" text-center border"> {{$buah->type_asrama}}</td>
-                                    @role('admin')
+                                    @role('super admin')
                                     <td class=" text-center py-1">
                                         <form action="/asrama/{{$buah->id}}" method="post">
                                             @csrf
@@ -109,6 +109,7 @@
                                                     <path stroke-linecap="round" stroke-linejoin="round" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" />
                                                 </svg></button>
                                         </form>
+
                                     </td>
                                     @endrole
                                 </tr>
