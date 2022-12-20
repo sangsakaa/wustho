95962c07964229288d74dee73536f6a14473f87a
sangsakaa <zainuri717@gmail.com>
2022-12-20 07:48:03 +0700

    nis kelas siswa

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -4,10 +4,7 @@
             <h2 class="text-xl font-semibold leading-tight">
                 {{ __('Dashboard') }}
             </h2>
-            <x-button target="_blank" href="https://github.com/kamona-wd/kui-laravel-breeze" variant="black" class="justify-center max-w-xs gap-2">
-                <x-icons.usercircle class="w-6 h-6" aria-hidden="true" />
-                <span>Star on Github</span>
-            </x-button>
+
         </div>
     </x-slot>
     <div class="px-6 py-2 overflow-hidden bg-white shadow-md dark:bg-dark-eval-1">
