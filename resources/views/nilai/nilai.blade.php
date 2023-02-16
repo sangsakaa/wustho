5f256b9dac813c99b1717cb4c9082d17c44fb68f
sangsakaa <zainuri717@gmail.com>
2023-02-17 06:20:57 +0700

    coba lagi

--- resources/views/nilai/nilai.blade.php
+++ resources/views/nilai/nilai.blade.php
@@ -40,10 +40,10 @@
                                     <button class=" bg-red-600 py-1 sm:w-full rounded-md text-white px-4 text-center capitalize">simpan nilai</button>
                                     <a href="/nilaimapel" class=" sm:w-full bg-red-600 py-1 rounded-md text-white px-4 text-center capitalize">Kembali</a>
                                     @endrole
-                                    @can('show post')
+                                    @role('guru')
                                     <button class=" bg-red-600 py-1 sm:w-full rounded-md text-white px-4 text-center capitalize">simpan nilai</button>
                                     <a href="/nilaiperguru" class=" sm:w-full bg-red-600 py-1 rounded-md text-white px-4 text-center capitalize">Kembali</a>
-                                    @endcan
+                                    @endrole
                                 </div>
                             </div>
                             <div class=" overflow-auto">
