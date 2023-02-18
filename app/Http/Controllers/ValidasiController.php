10a815c09166a57efa79ee0b3f82d0535882df77
sangsakaa <zainuri717@gmail.com>
2023-02-18 17:20:25 +0700

    coba lagi

--- app/Http/Controllers/ValidasiController.php
+++ app/Http/Controllers/ValidasiController.php
@@ -41,11 +41,19 @@ class ValidasiController
     }
     public function blangkoijazah(Siswa $siswa)
     {
-
+        $data = Pesertakelas::query()
+            ->join('siswa', 'siswa.id', '=', 'pesertakelas.siswa_id')
+            ->join('kelasmi', 'kelasmi.id', '=', 'pesertakelas.kelasmi_id')
+            ->join('nis', 'siswa.id', '=', 'nis.siswa_id')
+            ->leftjoin('statusanak', 'siswa.id', '=', 'statusanak.siswa_id')
+            ->leftjoin('statuspengamal', 'siswa.id', '=', 'statuspengamal.siswa_id')
+            ->where('kelasmi.periode_id', session('periode_id'))
+            ->get();
         return view(
             'validasi.blangkoijazah',
             [
-                'siswa' => $siswa   
+                'siswa' => $siswa,
+                'data' => $data,  
 
             ]
         );
--- resources/views/validasi/blangkoijazah.blade.php
+++ resources/views/validasi/blangkoijazah.blade.php
@@ -5,34 +5,47 @@
             {{ __('Dashboard blangko ijazah') }}
         </h2>
     </x-slot>
-    <div class=" px-36 py-10 bg-white  ">
-        <div class=" text-center  sm:text-center   rounded dark:bg-purple-600 ">
+    <div class="  px-36 py-10 bg-white  ">
+        @foreach($data as $ijazah)
+        <div class=" text-center    rounded dark:bg-purple-600 ">
             <div class=" grid justify-end">
                 <span class=" pr-10 mt-4"> Nomor : ...........................</span>
             </div>
             <center>
                 <img src={{ asset("asset/images/logo.png") }} alt="" width="200" class=" grayscale p-2">
                 <p class=" font-serif text-5xl  mt-10 font-semibold ">IJAZAH</p>
-                <p class=" capitalize  text-4xl  font-riqah py-2">المدرسه الدينيه الوسطى واحيديه</p>
+                <p class=" capitalize  text-4xl  font-riqah py-2">المدرسه الدينيه وسطى واحيديه</p>
                 <p class=" capitalize font-serif text-2xl">pondok pesantren kedunglo al munadhdhoroh</p>
-                <p class="  uppercase font-serif text-2xl font-semibold ">madrasah diniyah wustho wahidiyah</p>
+                <p class="  uppercase font-serif text-2xl font-semibold ">madrasah diniyah wustho
+                    Wahidiyah</p>
                 <p class=" capitalize font-serif text-2xl">kota kediri jawa timur indonesia</p>
             </center>
-            <p class="  text-left"> Yang bertanda tangan dibawah ini adalah Kepala Sekolah Madrasah Diniyah Wustho wahidiyah kedunglo Kediri menerangkan bahwa :</p>
-            <p class=" text-4xl uppercase bold  font-serif text-center mt-20 underline">muhammad bahrul ulum</p>
-            <p class=" text-lg uppercase font-semibold   font-sans text-center ">nomor induk siswa : 202002000001</p>
-
-            <div class=" px-1 ">Tempat, Tanggal Lahir</div>
-            <div class=" px-1 capitalize "> : {{strtolower($siswa->tempat_lahir)}},
-                {{ \Carbon\Carbon::parse($siswa->tanggal_lahir)->isoFormat(' DD MMMM Y') }}
+            <div>
+                <p class="  text-left mt-5"> Yang bertanda tangan dibawah ini Kepala Madrasah Diniyah Wustho Wahidiyah Kedunglo Kediri menerangkan bahwa :</p>
+                <p class=" text-3xl uppercase bold  font-serif text-center mt-15 underline">{{$ijazah->nama_siswa}}</p>
+                <p class=" text-lg uppercase font-semibold   font-sans text-center ">nomor induk siswa : {{$ijazah->nis}}</p>
             </div>
-            <div class=" px-1 ">Nama Orang Tua / Wali </div>
-            <div class=" px-1 capitalize "> : Mukidi
+
+            <div class=" text-left grid grid-cols-2 mt-5">
+                <div class=" px-1 ">Tempat, Tanggal Lahir</div>
+                <div class=" px-1 capitalize "> : {{strtolower($ijazah->tempat_lahir)}},
+                    {{ \Carbon\Carbon::parse($ijazah->tanggal_lahir)->isoFormat(' DD MMMM Y') }}
+                </div>
+                <div class=" px-1 ">Nama Orang Tua / Wali </div>
+                <div class=" px-1 capitalize "> : Mukidi
+                </div>
             </div>
             <p class=" text-4xl uppercase bold  font-serif text-center mt-4 ">lulus</p>
+            <p class=" text-justify ">dalam mengikuti Evaluasi Belajar Tahap Akhir Madrasah Diniyah Wustho Wahidiyah yang diselenggarakan pada tanggal................ s.d...................Dengan nilai dengan nilai sebagaimana tercantum pada daftar nilai dibalik ini.</p>
+
+            <p class=" text-justify mt-4">
+                Pemegang ijazah ini, terakhir tercatat sebagai siswa madrasah Diniyah wustho wahidiyah pondok pesantren kedunglo Kediri dengan <span class=" font-semibold">Nomor Induk Siswa</span> : {{$ijazah->nis}}
+            </p>
+
         </div>
+        @endforeach
     </div>
-    </div>
+
 
 
 </x-app-layout>
