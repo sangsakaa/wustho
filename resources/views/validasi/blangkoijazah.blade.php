2768f21eddf537035ffa2ea6c3cb3fce4596f0f7
sangsakaa <zainuri717@gmail.com>
2023-02-19 14:17:32 +0700

    akun guru

--- resources/views/validasi/blangkoijazah.blade.php
+++ resources/views/validasi/blangkoijazah.blade.php
@@ -17,6 +17,7 @@
     </script>
     <div class=" p-2">
         <button class=" text-white rounded-md  bg-green-800 px-2 py-1 " onclick="printContent('div1')">Cetak Ijazah</button>
+        <a href="/pengaturan" class=" text-white rounded-md  bg-green-800 px-2 py-1 ">Kembali</a>
     </div>
     <div id="div1" class=" px-4  ">
         <style>
@@ -25,7 +26,7 @@
             }
         </style>
         @foreach($data as $ijazah)
-        <div class=" max-[1414]: px-4 text-center  bg-white   rounded dark:bg-purple-600 ">
+        <div class=" px-28 py-20 text-center  bg-white   rounded dark:bg-purple-600 ">
             <div class=" grid justify-end">
                 <span class=" pr-10 mt-4"> Nomor : .......................................</span>
             </div>
@@ -34,15 +35,15 @@
                 <p class=" font-serif text-5xl  mt-10 font-semibold ">IJAZAH</p>
                 <p class=" capitalize  text-4xl  font-riqah py-2">ألمدرسة الدينية الوسطى الواحدية
                 </p>
-                <p class="  font-serif text-lg uppercase">pondok pesantren kedunglo al munadhdhoroh</p>
-                <p class="  uppercase font-serif text-2xl font-semibold ">madrasah diniyah wustho
+                <p class="  font-serif text-sm uppercase">pondok pesantren kedunglo al munadhdhoroh</p>
+                <p class="  uppercase font-serif text-lg font-semibold ">madrasah diniyah wustho
                     Wahidiyah</p>
                 <p class=" capitalize font-serif text-2xl">kota kediri jawa timur indonesia</p>
             </center>
             <div>
-                <p class=" text-justify   mt-5"> Yang bertanda tangan dibawah ini Kepala Madrasah Diniyah Wustho Wahidiyah Kedunglo Kediri menerangkan bahwa :</p>
-                <p class=" text-3xl uppercase bold  font-serif text-center mt-15 underline">{{$ijazah->nama_siswa}}</p>
-                <p class=" text-lg uppercase font-semibold   font-sans text-center ">nomor induk siswa : {{$ijazah->nis}}</p>
+                <p class=" text-justify   mt-5 text-sm"> Yang bertanda tangan dibawah ini Kepala Madrasah Diniyah Wustho Wahidiyah Kedunglo Kediri menerangkan bahwa :</p>
+                <p class=" text-2xl uppercase bold  font-serif text-center  mt-8 underline ">{{$ijazah->nama_siswa}}</p>
+                <p class=" text-sm uppercase font-semibold   font-sans text-center ">nomor induk siswa : {{$ijazah->nis}}</p>
             </div>
 
             <div class=" text-left grid grid-cols-2 mt-5">
@@ -51,22 +52,22 @@
                     {{ \Carbon\Carbon::parse($ijazah->tanggal_lahir)->isoFormat(' DD MMMM Y') }}
                 </div>
                 <div class=" px-1 ">Nama Orang Tua / Wali </div>
-                <div class=" px-1 capitalize "> : Mukidi
+                <div class=" px-1 capitalize "> : {{$ijazah->nama_ayah}}
                 </div>
             </div>
-            <p class=" text-4xl uppercase bold  font-serif text-center mt-4 ">lulus</p>
-            <p class=" text-justify ">Dalam mengikuti <span class=" font-semibold">Ujian Akhir Madrasah Diniyah Wustho Wahidiyah</span> yang diselenggarakan
+            <p class=" text-4xl uppercase bold  font-serif text-center mt-8 ">lulus</p>
+            <p class=" text-justify mt-8 ">Dalam mengikuti <span class=" font-semibold text-sm">Ujian Akhir Madrasah Diniyah Wustho Wahidiyah</span> yang diselenggarakan
                 pada tanggal................ s.d................... dengan nilai sebagaimana tercantum pada daftar nilai dibalik ini.</p>
 
-            <p class=" text-justify mt-4">
+            <p class=" text-justify mt-4 text-sm">
                 Pemegang ijazah ini, terakhir tercatat sebagai <span class=" capitalize">siswa madrasah Diniyah wustho wahidiyah pondok pesantren kedunglo Kediri</span> dengan <span class=" font-semibold">Nomor Induk Siswa</span> : {{$ijazah->nis}}
             </p>
 
-            <div class="  flex grid-cols-2 text-right mt-4">
+            <div class="  flex grid-cols-2 text-right mt-4 text-sm">
                 <div class=" w-2/3"></div>
                 <div class="  text-left">
                     Kedunglo, {{ now()->isoFormat('D MMMM YYYY') }}<br>
-                    Al Mudir / Kepala <br><br><br><br>
+                    Kepala Madrasah<br><br><br><br>
                     <p class=" uppercase"> Muh. Bahrul Ulum, S.H</p>
                 </div>
             </div>
