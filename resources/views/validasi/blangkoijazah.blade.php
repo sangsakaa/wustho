1a989209600f631f42c4fbf6b6237287aa756917
sangsakaa <zainuri717@gmail.com>
2023-03-06 08:04:41 +0700

    ijazah

--- resources/views/validasi/blangkoijazah.blade.php
+++ resources/views/validasi/blangkoijazah.blade.php
@@ -93,8 +93,9 @@
 
                     <p class="  px-20 ml-1">19 Maret 2023 M</p>
                     <p class="">Mengetahui,</p>
-                    <p>Pengasuh Perjuangan Wahidiyah</p>
-                    <p>Dan Pondok Pesantren Kedunglo </p>
+                    <p>Pengasuh Pondok</p>
+                    <!-- <p>Pengasuh Perjuangan Wahidiyah</p> -->
+                    <!-- <p>Dan Pondok Pesantren Kedunglo </p> -->
 
 
                     <br><br><br><br><br>
