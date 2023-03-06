bf6b52712322e76a5e09a63e278e36f505bff3ae
sangsakaa <zainuri717@gmail.com>
2023-03-06 08:02:14 +0700

    ijazah

--- resources/views/validasi/blangkoijazah.blade.php
+++ resources/views/validasi/blangkoijazah.blade.php
@@ -91,7 +91,7 @@
                         <!-- {{ \Carbon\Carbon::parse($ijazah->tanggal_kelulusan)->isoFormat(' DD MMMM Y') }}</p> -->
                     <p class=" underline"> Kedunglo, 27 Sya'ban 1444 H </p>
 
-                    <p class="  px-20">19 Maret 2023 M</p>
+                    <p class="  px-20 ml-1">19 Maret 2023 M</p>
                     <p class="">Mengetahui,</p>
                     <p>Pengasuh Perjuangan Wahidiyah</p>
                     <p>Dan Pondok Pesantren Kedunglo </p>
