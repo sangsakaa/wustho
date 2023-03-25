ca4fd90119fdf16ae3221ec86b28acd9b6a1253d
sangsakaa <zainuri717@gmail.com>
2023-03-26 06:35:40 +0700

    nilai akhir

--- app/Http/Controllers/PararelController.php
+++ app/Http/Controllers/PararelController.php
@@ -12,6 +12,61 @@ class PararelController
 {
     public function index()
     {
+        // $nilaiPerPesertaKelas = Nilai::query()
+        //     ->join('pesertakelas', 'pesertakelas.id', '=', 'nilai.pesertakelas_id')
+        //     ->join('kelasmi', 'kelasmi.id', '=', 'pesertakelas.kelasmi_id')
+        //     ->join('kelas', 'kelas.id', '=', 'kelasmi.kelas_id')
+        //     ->join('siswa', 'siswa.id', '=', 'pesertakelas.siswa_id')
+        //     ->join('nilaimapel', 'nilaimapel.id', '=', 'nilai.nilaimapel_id')
+        //     ->join('mapel', 'mapel.id', '=', 'nilaimapel.mapel_id')
+        //     ->select('siswa.nama_siswa', 'kelas.kelas', 'mapel.mapel', 'nilai.nilai_harian', 'nilai.nilai_ujian', 'nilai.pesertakelas_id')
+        //     ->where('kelasmi.periode_id', session('periode_id'))
+        //     ->where('kelas.kelas', 3)
+        //     ->get()
+        // ->groupBy('pesertakelas_id');
+        // $nilaiPesertaKelasMap = collect();
+        
+        // $nilaiPerPesertaKelas->each(function ($items, $pesertaKelasId) use ($nilaiPesertaKelasMap) {
+        //     $nilaiPesertaKelas = ['pesertakelas_id' => $pesertaKelasId];
+        //     $items->each(function ($item) use (&$nilaiPesertaKelas) {
+        //         $namaSiswa = $item->nama_siswa;
+        //         $mapel = $item->mapel;
+        //         $nilaiHarian = $item->nilai_harian;
+        //         $nilaiUjian = $item->nilai_ujian;
+        //         if (!isset($nilaiPesertaKelas[$namaSiswa])) {
+        //             $nilaiPesertaKelas[$namaSiswa] = [];
+        //         }
+        //         if (!isset($nilaiPesertaKelas[$namaSiswa][$mapel])) {
+        //             $nilaiPesertaKelas[$namaSiswa][$mapel] = [];
+        //         }
+        //         $nilaiPesertaKelas[$namaSiswa][$mapel]['nilaiHarian'] = $nilaiHarian;
+        //         $nilaiPesertaKelas[$namaSiswa][$mapel]['nilaiUjian'] = $nilaiUjian;
+        //     });
+        //     $nilaiPesertaKelasMap->push($nilaiPesertaKelas);
+            
+        // });
+
+        // $siswa = Pesertakelas::query()
+        // ->join('siswa', 'siswa.id', '=', 'pesertakelas.siswa_id')
+        // ->join('kelasmi', 'kelasmi.id', '=', 'pesertakelas.kelasmi_id')
+        //     ->join('kelas', 'kelas.id', '=', 'kelasmi.kelas_id')
+        // ->where('kelasmi.periode_id', session('periode_id'))
+        //     ->select('pesertakelas.id', 'nama_siswa', 'kelas', 'nama_kelas')
+        // ->orderby('kelasmi.nama_kelas')
+        // ->orderby('siswa.nama_siswa')
+        //     ->where('kelas', 3)
+        // ->get();
+        // $mapel = Mapel::query()
+        // ->join('kelas', 'kelas.id', '=', 'mapel.kelas_id')
+        // ->select('mapel.id', 'mapel', 'kelas')
+        //     ->where('kelas', 3)
+        // ->orderbY('kelas.kelas')
+        // ->get();
+        // return view('seleksi.nominasi.index', [
+        //     'nilaiPesertaKelasMap' => $nilaiPesertaKelasMap,
+        //     'siswa' => $siswa,
+        //     'mapel' => $mapel,
+        // ]);
         $nilaiPerPesertaKelas = Nilai::query()
             ->join('pesertakelas', 'pesertakelas.id', '=', 'nilai.pesertakelas_id')
             ->join('kelasmi', 'kelasmi.id', '=', 'pesertakelas.kelasmi_id')
@@ -24,26 +79,31 @@ class PararelController
             ->where('kelas.kelas', 3)
             ->get()
         ->groupBy('pesertakelas_id');
+
         $nilaiPesertaKelasMap = collect();
-        
+
         $nilaiPerPesertaKelas->each(function ($items, $pesertaKelasId) use ($nilaiPesertaKelasMap) {
             $nilaiPesertaKelas = ['pesertakelas_id' => $pesertaKelasId];
+
             $items->each(function ($item) use (&$nilaiPesertaKelas) {
                 $namaSiswa = $item->nama_siswa;
                 $mapel = $item->mapel;
                 $nilaiHarian = $item->nilai_harian;
                 $nilaiUjian = $item->nilai_ujian;
+
                 if (!isset($nilaiPesertaKelas[$namaSiswa])) {
                     $nilaiPesertaKelas[$namaSiswa] = [];
                 }
+
                 if (!isset($nilaiPesertaKelas[$namaSiswa][$mapel])) {
                     $nilaiPesertaKelas[$namaSiswa][$mapel] = [];
                 }
+
                 $nilaiPesertaKelas[$namaSiswa][$mapel]['nilaiHarian'] = $nilaiHarian;
                 $nilaiPesertaKelas[$namaSiswa][$mapel]['nilaiUjian'] = $nilaiUjian;
             });
+
             $nilaiPesertaKelasMap->push($nilaiPesertaKelas);
-            
         });
 
         $siswa = Pesertakelas::query()
@@ -51,22 +111,26 @@ class PararelController
         ->join('kelasmi', 'kelasmi.id', '=', 'pesertakelas.kelasmi_id')
             ->join('kelas', 'kelas.id', '=', 'kelasmi.kelas_id')
         ->where('kelasmi.periode_id', session('periode_id'))
+            ->where('kelas.kelas', 3)
             ->select('pesertakelas.id', 'nama_siswa', 'kelas', 'nama_kelas')
-        ->orderby('kelasmi.nama_kelas')
-        ->orderby('siswa.nama_siswa')
-            ->where('kelas', 3)
+            ->orderBy('kelasmi.nama_kelas')
+            ->orderBy('siswa.nama_siswa')
         ->get();
+
         $mapel = Mapel::query()
         ->join('kelas', 'kelas.id', '=', 'mapel.kelas_id')
-        ->select('mapel.id', 'mapel', 'kelas')
             ->where('kelas', 3)
-        ->orderbY('kelas.kelas')
+        ->select('mapel.id', 'mapel', 'kelas')
+            ->orderBy('kelas.kelas')
         ->get();
+
         return view('seleksi.nominasi.index', [
             'nilaiPesertaKelasMap' => $nilaiPesertaKelasMap,
             'siswa' => $siswa,
             'mapel' => $mapel,
         ]);
+   
+
 
     }
 }
--- resources/views/seleksi/nominasi/index.blade.php
+++ resources/views/seleksi/nominasi/index.blade.php
@@ -9,38 +9,52 @@
         </div>
     </x-slot>
     <div class=" bg-white   px-2 py-2 gap-2">
-        <table>
+        <table class=" w-full">
             <thead>
-                <tr class=" border text-xs uppercase">
-                    <th class=" border  w-10   h-40  ">No</th>
-                    <th class=" border   ">Siswa</th>
-                    <th class=" border -rotate-90   ">Kelas</th>
-                    @foreach ($mapel as $m)
-                    <th class=" border -rotate-90   ">{{ $m->mapel }}</th>
+                <tr class=" border text-sm">
+                    <th class=" border">No</th>
+                    <th class=" border">Nama Siswa</th>
+                    <th class=" border -rotate-90">Kelas</th>
+                    <th class=" border -rotate-90">Nama Kelas</th>
+                    @foreach($mapel as $m)
+                    <th class=" rotate-90  h-48  border">{{ $m->mapel }}</th>
                     @endforeach
+                    <th class=" border -rotate-90">Status</th>
                 </tr>
             </thead>
             <tbody>
-                @foreach ($siswa as $s)
-                <tr class=" border">
-                    <td class=" text-center ">{{ $loop->iteration }}</td>
-                    <td class=" border w-1/4 capitalize px-1">{{ strtolower($s->nama_siswa) }}</td>
-                    <td class="  w-5 text-center">{{$s->nama_kelas}}</td>
-                    @foreach ($mapel as $m)
-                    <td class=" border text-center  text-sm font-semibold">
-                        @if (isset($nilaiPesertaKelasMap->where('pesertakelas_id', $s->id)->first()[$s->nama_siswa][$m->mapel]))
-                        {{ $nilaiPesertaKelasMap->where('pesertakelas_id', $s->id)->first()[$s->nama_siswa][$m->mapel]['nilaiHarian'] }}
-                        /
-                        {{ $nilaiPesertaKelasMap->where('pesertakelas_id', $s->id)->first()[$s->nama_siswa][$m->mapel]['nilaiUjian'] }}
-                        @else
-                        <span class=" text-center  text-red-600  ">X</span>
+                @foreach($siswa as $s)
+                <tr class="border even:bg-gray-100">
+                    <td class=" text-center">{{$loop->iteration}}</td>
+                    <td class="border px-2  capitalize">{{ strtolower($s->nama_siswa) }}</td>
+                    <td class="border px-2 text-center">{{ $s->kelas }}</td>
+                    <td class="border px-2 text-center">{{ $s->nama_kelas }}</td>
+                    @php
+                    $totalNilai = 0;
+                    @endphp
+                    @foreach($mapel as $m)
+                    <td class=" text-center border px-1 w-15  ">
+                        @foreach($nilaiPesertaKelasMap as $n)
+                        @if($n['pesertakelas_id'] == $s->id && isset($n[$s->nama_siswa][$m->mapel]))
+                        @php
+                        $nilaiHarian = $n[$s->nama_siswa][$m->mapel]['nilaiHarian'];
+                        $nilaiUjian = $n[$s->nama_siswa][$m->mapel]['nilaiUjian'];
+                        $totalNilai += $nilaiHarian + $nilaiUjian;
+                        @endphp
+                        <span class="{{ ($nilaiHarian < 65 || $nilaiUjian < 65) ? 'text-red-500' : '' }}">
+                            {{ $nilaiHarian }} / {{ $nilaiUjian }}
+                        </span>
                         @endif
+                        @endforeach
                     </td>
                     @endforeach
+                    <td class="   text-center ">
+                        @if($totalNilai < 800) <span class=" text-red-600"> Tidak| {{ $totalNilai }}</span> @else Lulus | {{ $totalNilai }} @endif </td>
                 </tr>
                 @endforeach
             </tbody>
         </table>
+
 
 
 
