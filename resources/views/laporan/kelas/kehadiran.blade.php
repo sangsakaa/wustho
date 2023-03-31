fca6f0ada8c6bb1b044f2ce4b722ca18c6dec329
sangsakaa <zainuri717@gmail.com>
2023-04-01 06:54:47 +0700

    grafik bar

--- resources/views/laporan/kelas/kehadiran.blade.php
+++ resources/views/laporan/kelas/kehadiran.blade.php
@@ -8,9 +8,7 @@
 
         </div>
     </x-slot>
-    <div>
-        {{$lapKehadiran}}
-    </div>
+
     <div class=" bg-white grid sm:grid-cols-2 grid-cols-1 px-2 py-2 gap-2">
 
         <canvas id="chart"></canvas>
