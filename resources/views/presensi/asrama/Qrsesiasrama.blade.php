0ac8fa4028cd289658b3764938f261c3a49ce465
sangsakaa <zainuri717@gmail.com>
2023-05-21 11:18:26 +0700

    size camera

--- resources/views/presensi/asrama/Qrsesiasrama.blade.php
+++ resources/views/presensi/asrama/Qrsesiasrama.blade.php
@@ -10,31 +10,42 @@
   <div class="px-4 mt-4 ">
     <div class=" bg-white overflow-hidden shadow-sm sm:rounded-lg">
       <div class="p-2 ">
-        <div class=" w-full sm:w-1/2 h-96">
+        <div class="w-full sm:w-1/2 h-96">
           <h1 class="text-center">QR Code Reader</h1>
-          <video id="video" width="450px"></video>
-          <canvas id="canvas" width="450px" style="display:none;"></canvas>
-          <div id="result"></div>
+          <div id="video-container">
+            <video id="video" width="450px"></video>
+            <canvas id="canvas" width="450px" style="display:none;"></canvas>
+            <div id="result"></div>
+          </div>
           <script src="https://cdn.jsdelivr.net/npm/qr-scanner"></script>
           <script>
             window.onload = function() {
               const video = document.getElementById("video");
               const canvas = document.getElementById("canvas");
               const resultDiv = document.getElementById("result");
+              const videoContainer = document.getElementById("video-container");
 
               // Menginisialisasi scanner QR
               const scanner = new QrScanner(video, result => {
                 resultDiv.innerText = result;
                 scanner.stop();
+                videoContainer.classList.add("qr-detected");
               });
 
               // Mengaktifkan kamera saat halaman selesai dimuat
               scanner.start();
             };
           </script>
-
+          <style>
+            .qr-detected {
+              border: 2px solid green;
+            }
+          </style>
         </div>
+
+
       </div>
     </div>
+  </div>
 
 </x-app-layout>
