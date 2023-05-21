c3dd0d43dba90b330d5c8d3392db8c9954be6978
sangsakaa <zainuri717@gmail.com>
2023-05-21 10:49:47 +0700

    size camera

--- resources/views/presensi/asrama/Qrsesiasrama.blade.php
+++ resources/views/presensi/asrama/Qrsesiasrama.blade.php
@@ -12,29 +12,36 @@
       <div class="p-2 ">
         <div class=" w-full sm:w-1/2">
 
-          <h1 class=" text-center">QR Code Reader</h1>
-          <video id="video" width="100%"></video>
-          <canvas id="canvas" style="display:none;"></canvas>
+          <h1 class="text-center">QR Code Reader</h1>
+          <video id="video" style="width: 100%; height: auto;"></video>
+          <canvas id="canvas" style="display: none;"></canvas>
           <div id="result"></div>
-        </div>
-        <script src="https://cdn.jsdelivr.net/npm/qr-scanner"></script>
-        <script>
-          window.onload = function() {
-            const video = document.getElementById("video");
-            const canvas = document.getElementById("canvas");
-            const resultDiv = document.getElementById("result");
 
-            // Menginisialisasi scanner QR
-            const scanner = new QrScanner(video, result => {
-              resultDiv.innerText = result;
-              scanner.stop();
-            });
-            // Mengaktifkan kamera saat halaman selesai dimuat
-            scanner.start();
-          };
-        </script>
+          <script src="https://cdn.jsdelivr.net/npm/qr-scanner"></script>
+          <script>
+            window.onload = function() {
+              const video = document.getElementById("video");
+              const canvas = document.getElementById("canvas");
+              const resultDiv = document.getElementById("result");
+
+              // Menginisialisasi scanner QR
+              const scanner = new QrScanner(video, result => {
+                resultDiv.innerText = result;
+                scanner.stop();
+              });
+
+              // Mengaktifkan kamera saat halaman selesai dimuat
+              scanner.start();
+
+              // Mengatur proporsi video di perangkat seluler
+              if (window.innerWidth <= 600) { // Ubah angka 600 sesuai dengan batas ukuran layar yang diinginkan
+                video.style.height = "50%";
+              }
+            };
+          </script>
+
+        </div>
       </div>
     </div>
-  </div>
 
 </x-app-layout>
