70aca7e0bf135be77bcb7cea4a849bb60302f334
sangsakaa <zainuri717@gmail.com>
2023-05-21 10:29:50 +0700

    ok

--- resources/views/presensi/asrama/Qrsesiasrama.blade.php
+++ resources/views/presensi/asrama/Qrsesiasrama.blade.php
@@ -10,13 +10,14 @@
   <div class="px-4 mt-4 ">
     <div class=" bg-white overflow-hidden shadow-sm sm:rounded-lg">
       <div class="p-2 ">
-        <h1>QR Code Reader</h1>
-        <video id="video" width="100%" height="auto"></video>
-        <canvas id="canvas" style="display:none;"></canvas>
-        <div id="result"></div>
+        <div class=" w-full sm:w-1/2">
+          <h1 class=" text-center">QR Code Reader</h1>
+          <video id="video" height="100%" width="100%"></video>
 
+          <canvas id="canvas" style="display:none;"></canvas>
+          <div id="result"></div>
+        </div>
         <script src="https://cdn.jsdelivr.net/npm/qr-scanner"></script>
-
         <script>
           window.onload = function() {
             const video = document.getElementById("video");
@@ -28,7 +29,6 @@
               resultDiv.innerText = result;
               scanner.stop();
             });
-
             // Mengaktifkan kamera saat halaman selesai dimuat
             scanner.start();
           };
