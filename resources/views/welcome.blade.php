f1734636b6bcd8d9376a57537ddc5d33e1ddbde3
sangsakaa <zainuri717@gmail.com>
2023-05-22 08:06:52 +0700

    ok

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,7 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Beranda</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
-
+    <script src="https://cdn.tailwindcss.com"></script>
+    <script src="https://cdn.tailwindcss.com?plugins=forms,typography,aspect-ratio,line-clamp"></script>
 </head>
 
 <body>
@@ -16,17 +17,18 @@
                 <img src="asset/images/logo.png" alt="" width="50px"><br>
             </a>
             <ul class=" mx-4">
-                <a class="dropdown-item active " href="{{ route('login') }}">Masuk</a>
+                <a class="dropdown-item active py-2" href="{{ route('login') }}">Masuk</a>
             </ul>
         </div>
+
     </nav>
-    <div class=" bg-white text-black" data-bs-theme="dark">
-        <table class=" mt-2">
+    <div class="dropdown" data-bs-theme="dark">
+        <table>
             <thead>
                 <tr>
-                    <th class=" border px-1">Nama Kelas</th>
-                    <th class=" border px-1">Nama Siswa</th>
-                    <th class=" border px-1">Total Alfa</th>
+                    <th>Nama Kelas</th>
+                    <th>Nama Siswa</th>
+                    <th>Total Alfa</th>
                 </tr>
             </thead>
             <tbody>
@@ -36,13 +38,13 @@
                 @foreach ($data as $item)
                 <tr>
                     @if ($item->nama_kelas != $prevKelas)
-                    <td class=" px-1 border text-center " rowspan="6">{{ $item->nama_kelas }}</td>
+                    <td class="border " rowspan="6">{{ $item->nama_kelas }}</td>
                     @php
                     $prevKelas = $item->nama_kelas;
                     @endphp
                     @endif
-                    <td class=" px-1 border text-left ">{{ $item->nama_siswa }}</td>
-                    <td class=" px-1 border text-center ">{{ $item->total_alfa }}</td>
+                    <td class="border ">{{ $item->nama_siswa }}</td>
+                    <td class="border ">{{ $item->total_alfa }}</td>
                 </tr>
                 @endforeach
             </tbody>
--- routes/web.php
+++ routes/web.php
@@ -366,7 +366,7 @@ Route::get(
             ->join('pesertakelas', 'pesertakelas.id', '=', 'absensikelas.pesertakelas_id')
             ->join('siswa', 'siswa.id', '=', 'pesertakelas.siswa_id')
             ->join('kelasmi', 'kelasmi.id', '=', 'pesertakelas.kelasmi_id')
-            // ->where('kelasmi.periode_id', session('periode_id'))
+            ->where('kelasmi.periode_id', session('periode_id'))
             ->where('absensikelas.keterangan', 'alfa')
             ->groupBy('nama_kelas', 'nama_siswa') // Menambahkan kolom nama_siswa pada grup
             ->select('nama_kelas', 'nama_siswa', DB::raw('count(*) as total_alfa')) // Menambahkan kolom nama_siswa pada select
