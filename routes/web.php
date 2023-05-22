cd6f2369d0a841856daf93c4969736009582a852
sangsakaa <zainuri717@gmail.com>
2023-05-23 05:34:52 +0700

    ok

--- routes/web.php
+++ routes/web.php
@@ -374,14 +374,22 @@ Route::get(
             ->join('pesertakelas', 'pesertakelas.id', '=', 'absensikelas.pesertakelas_id')
             ->join('siswa', 'siswa.id', '=', 'pesertakelas.siswa_id')
             ->join('kelasmi', 'kelasmi.id', '=', 'pesertakelas.kelasmi_id')
-             // Ambil periode terakhir dari session
-            ->where('absensikelas.keterangan', 'alfa')
+            // Ambil periode terakhir dari session
+            ->whereIn('absensikelas.keterangan', ['alfa', 'sakit'])
             ->groupBy('nama_kelas', 'nama_siswa', 'periode_id')
-            ->select('nama_kelas', 'nama_siswa', 'periode_id', DB::raw('count(*) as total_alfa'), DB::raw('count(pesertakelas.id) as total_alfa'))
+            ->select(
+                'nama_kelas',
+                'nama_siswa',
+                'periode_id',
+                DB::raw('SUM(CASE WHEN absensikelas.keterangan = "alfa" THEN 1 ELSE 0 END) as total_alfa'),
+                DB::raw('SUM(CASE WHEN absensikelas.keterangan = "sakit" THEN 1 ELSE 0 END) as total_sakit'),
+                DB::raw('count(pesertakelas.id) as total_data')
+            )
             ->orderBy('nama_kelas')
             ->orderBy('nama_siswa')
             ->where('kelasmi.periode_id', $kelasmi->id)
         ->get();
+
 
         // dd($data);
 
