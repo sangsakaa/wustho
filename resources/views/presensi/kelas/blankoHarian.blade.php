a38c56e4909f600168a16fb66bcdb4449099fd38
sangsakaa <zainuri717@gmail.com>
2023-05-28 17:23:54 +0700

    ok fix

--- resources/views/presensi/kelas/blankoHarian.blade.php
+++ resources/views/presensi/kelas/blankoHarian.blade.php
@@ -25,16 +25,17 @@
             document.body.innerHTML = fullbody;
         }
     </script>
-    <style>
-        .page-break {
-            page-break-after: always;
-        }
-    </style>
+
     <div class="py-1" id="blanko">
+        <style>
+            .page-break {
+                page-break-after: always;
+            }
+        </style>
         <div class=" bg-white dark:bg-dark-bg overflow-hidden shadow-sm">
             <div class=" px-2  grid grid-cols-2 gap-2">
                 @foreach ($kelasmi as $item)
-                <div class="   ">
+                <div class=" ">
                     <div class=" overflow-auto bg-white dark:bg-dark-bg block sm:hidden ">
                         <div class=" text-center text-green-900  ">
                             <p class=" font-semibold text-sm">
@@ -81,8 +82,67 @@
                     </table>
                 </div>
                 @endforeach
+
+            </div>
+            <div class="page-break "></div>
+            <div class=" grid grid-cols-2 gap-1">
+                @foreach ($kelasmi as $item)
+                <div class=" text-green-800 ">
+                    <div class=" overflow-auto bg-white dark:bg-dark-bg block sm:hidden ">
+                        <div class=" text-center text-green-900  ">
+                            <p class=" font-semibold text-sm">
+                                MADRASAH DINIYAH WUSTHO WAHIDIYAH
+                            </p>
+                            <p class=" font-semibold uppercase text-sm">
+                                TAHUN PELAJARAN {{$item->periode}} {{$item->ket_semester}}
+                            </p>
+                        </div>
+                        <hr class=" border-b-2 border-green-900">
+                    </div>
+                    <div class=" flex grid-cols-4 font-semibold text-xs gap-5 py-1">
+                        <div class=" w-20">Kelas</div>
+                        <div>: {{$item->nama_kelas}}</div>
+                        <div>Tanggal</div>
+                        <div>: .......... {{$bulan }}</div>
+                    </div>
+                    <div class=" text-center">
+                        <p class=" uppercase text-xs font-semibold">izin mengikuti/meninggalkan pelajaran</p>
+                        <hr>
+                    </div>
+                    <div class=" grid grid-cols-2">
+                        <div class=" py-1">Nama</div>
+                        <div class=" py-1 border-b-2 ">:</div>
+                        <div class=" py-1">Kelas</div>
+                        <div class=" py-1 border-b-2 ">:</div>
+                        <div class=" py-1">Asrama</div>
+                        <div class=" py-1 border-b-2 ">:</div>
+                    </div>
+                    <div class=" ">
+                        <p class="  capitalize text-xs">untuk mengikuti/meninggalkan pelajaran karena</p>
+                        <div class="">
+                            <input type="checkbox" name="" id=""> Sakit
+                            <input type="checkbox" name="" id=""> Izin
+                            <div class=" h-20 border border-green-800">
+                                <p class="px-1">Alasan :</p>
+                                <p></p>
+                            </div>
+                            <div class=" grid grid-cols-2">
+                                <div class="  text-center">
+                                    <p>Ketua Asrama,</p>
+                                    <p class=" mt-16">..............</p>
+                                </div>
+                                <div class=" text-center">
+                                    <p>Mentor,</p>
+                                    <p class=" mt-16">..............</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                @endforeach
                 <div class="page-break "></div>
             </div>
+
         </div>
 
 </x-app-layout>
