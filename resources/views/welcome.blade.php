aa0d1f0bb87ce175684a75e98dd17fbe0ff3f9e0
sangsakaa <zainuri717@gmail.com>
2023-07-20 18:33:20 +0700

    ok

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -17,13 +17,13 @@
             <div class=" flex grid-cols-1">
                 <div>
                     <a class="navbar-brand" href="/">
-                        <img src="asset/images/logo.png" alt="" width="90px">
+                        <img src="asset/images/logo.png" alt="" width="70px">
                     </a>
                 </div>
                 <div class=" py-1 px-2">
                     <p class=" text-lg">NIMW</p>
                     <p class="  text-blue-600">Nomor Induk Murid Wustho</p>
-                    <p>Alamat : Pondok Pesantren Kedunglo
+                    <p class=" text-xs">Alamat : Pondok Pesantren Kedunglo
                         Jl.KH. Wachid Hasyim Kota Kediri
                         Jawa Timur
                     </p>
@@ -38,27 +38,28 @@
     </div>
     <!-- <div class="dropdown" data-bs-theme="dark"> -->
     <div>
-        <div class=" grid grid-cols-1 sm:grid-cols-2 p-2">
-            <div>
+        <div class=" grid grid-cols-1 sm:grid-cols-2 p-2 gap-2">
+            <div class=" h-32 rounded-md bg-blue-300 p-2">
                 <form action="/" method="get" class="  text-sm gap-1 ">
                     <div class=" w-full px-4 gap-2">
+                        <span class=" uppercase">Pencarian NIMW (Nomor induk murid WUSTHO)</span>
                         <div class=" py-1">
                             <input type="text" max="8" name="cari" value="{{ request('cari') }}" class=" border w-full  py-2 px-2 " placeholder=" Masukan NIMW : 20220200109" autofocus>
                         </div>
-                        <button type="submit" class=" px-2 py-2 rounded-md    bg-blue-500   text-white">
+                        <button type="submit" class=" hover:bg-blue-800 px-2 py-2 rounded-md    bg-blue-500   text-white">
                             Cari By NIMW </button>
                     </div>
                 </form>
             </div>
-            <div>
+            <div class=" bg-blue-200 p-2 rounded-md">
                 <div class=" sm:flex grid ">
                     <div>
-                        <img src="asset/images/logo.png" alt="" width="80px">
+                        <img src="asset/images/logo.png" alt="" width="70px">
                     </div>
-                    <div class=" px-2">
-                        <p>MADRASAH DINIYAH WAHIDIYAH</p>
-                        <p class=" text-xs sm:text-lg">MADRASAH DINIYAH WUSTHO WAHIDIYAH</p>
-                        <p>Alamat : Pondok Pesantren Kedunglo
+                    <div class=" px-2 mt-1">
+                        <p class=" text-sm">MADRASAH DINIYAH WAHIDIYAH</p>
+                        <p class=" text-xs sm:text-sm">MADRASAH DINIYAH WUSTHO WAHIDIYAH</p>
+                        <p class=" text-sm">Alamat : Pondok Pesantren Kedunglo
                             Jl.KH. Wachid Hasyim Kota Kediri
                             Jawa Timur
                         </p>
