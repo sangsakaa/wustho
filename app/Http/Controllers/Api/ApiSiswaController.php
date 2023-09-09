fb4bad601f5371f6ccad25c8f57854c31ef11b05
sangsakaa <zainuri717@gmail.com>
2023-09-09 17:46:57 +0700

    apirekap

--- app/Http/Controllers/Api/ApiSiswaController.php
+++ app/Http/Controllers/Api/ApiSiswaController.php
@@ -72,7 +72,7 @@ class ApiSiswaController
             })
             ->select('kelasmi.jenjang', 'peserta_asrama.nama_asrama', 'absensikelas.id As id_sesi_kelas', 'siswa.nama_siswa', 'absensikelas.keterangan', 'nama_kelas', 'tgl')
             // ->where('sesikelas.tgl', $tgl->toDateString())
-            ->whereIn('keterangan', ['sakit', 'izin', 'alfa'])
+            ->whereIn('keterangan', ['sakit', 'izin', 'alfa', 'hadir'])
             
             ->orderBy('peserta_asrama.nama_asrama')
             ->orderBy('kelasmi.nama_kelas')
