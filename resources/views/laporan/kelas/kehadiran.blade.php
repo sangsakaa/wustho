fd9059610bd107d91970fb005fbf5b6ff1ccc142
sangsakaa <zainuri717@gmail.com>
2023-09-25 17:16:23 +0700

    ttd

--- resources/views/laporan/kelas/kehadiran.blade.php
+++ resources/views/laporan/kelas/kehadiran.blade.php
@@ -190,7 +190,7 @@
                                     {{$item->total_izin}}
                                 </td>
                                 <td class="text-center border">
-                                    {{number_format($item->total_alfa / $item->total_kehadiran + $item->total_alfa + $item->total_sakit +$item->total_izin *100,0)}}
+                                    {{number_format($item->total_alfa / $item->total_kehadiran + $item->total_alfa + $item->total_sakit +$item->total_izin,0)}}
                                     %
                                 </td>
 
