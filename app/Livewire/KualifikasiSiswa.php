9c47ca86e8a9f1c15d8b39070edacfe9eb1b4c36
sangsakaa <zainuri717@gmail.com>
2023-11-06 10:03:08 +0700

    lager presensi

--- app/Livewire/KualifikasiSiswa.php
+++ app/Livewire/KualifikasiSiswa.php
@@ -8,17 +8,18 @@ use Livewire\Component;
 
 class KualifikasiSiswa extends Component
 {
+    public $search = '';
     public function render()
     {
-        $dataSiswa = Pesertakelas::query()
+        $dataSiswa = Pesertakelas::search($this->search)
             ->join('absensikelas', 'absensikelas.pesertakelas_id', 'pesertakelas.id')
             ->join('siswa', 'pesertakelas.siswa_id', 'siswa.id')
             ->join('nis', 'siswa.id', 'nis.siswa_id')
             ->join('kelasmi', 'pesertakelas.kelasmi_id', 'kelasmi.id')
             ->join('periode', 'kelasmi.periode_id', 'periode.id')
-            ->select('siswa.nama_siswa', 'periode.periode', 'nis')
+            ->select('siswa.nama_siswa', 'periode.periode', 'nis', 'nama_kelas')
             ->selectRaw('SUM(CASE WHEN absensikelas.keterangan = "hadir" THEN 1 ELSE 0 END) as kehadiran')
-            ->groupBy('siswa.nama_siswa', 'periode.periode', 'nis')
+            ->groupBy('siswa.nama_siswa', 'periode.periode', 'nis', 'nama_kelas')
             ->orderby('nis.nis')
             ->get();
 
--- resources/views/livewire/kualifikasi-siswa.blade.php
+++ resources/views/livewire/kualifikasi-siswa.blade.php
@@ -1,10 +1,12 @@
 <div>
+    <input type="search" wire:model="search" class=" py-1 " placeholder=" cari nama kelas">
     <table class="w-full">
         <thead>
             <tr>
                 <th class="border">No</th>
                 <th class="border">NIM</th>
                 <th class="border">Nama Siswa</th>
+                <th class="border">Kelas</th>
                 @foreach($dataSiswa->pluck('periode')->unique() as $periode)
                 <th class="border">{{ $periode }}</th>
                 @endforeach
@@ -13,9 +15,10 @@
         <tbody>
             @foreach($dataSiswa->unique('nama_siswa') as $siswa)
             <tr>
-                <td class="border">{{ $loop->iteration }}</td>
+                <td class="border text-center">{{ $loop->iteration }}</td>
                 <td class="border text-center">{{ $siswa->nis }}</td>
-                <td class="border">{{ $siswa->nama_siswa }}</td>
+                <td class="border capitalize">{{ strtolower($siswa->nama_siswa )}}</td>
+                <td class="border text-center">{{ $siswa->nama_kelas }}</td>
                 @foreach($dataSiswa->pluck('periode')->unique() as $periode)
                 @php
                 $kehadiran = $dataSiswa->where('nama_siswa', $siswa->nama_siswa)->where('periode', $periode)->first();
