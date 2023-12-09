8b3d01da470c5121cfccabdf8162f68919d628cf
sangsakaa <zainuri717@gmail.com>
2023-12-09 08:36:24 +0700

    layout

--- resources/views/validasi/blangkoijazah.blade.php
+++ resources/views/validasi/blangkoijazah.blade.php
@@ -101,7 +101,7 @@
                                 <p class="">Foto 3x4</p>
                             </span>
                         </div>
-                        <div class="   grid   text-left py-8  ">
+                        <div class="   grid   text-left  mt-4  ">
                             <div class="flex flex-col">
                                 <table class="  w-fit">
                                     <tbody>
