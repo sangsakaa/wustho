da3d6dd672512b4a6e5130b8bb0799a415df959f
sangsakaa <zainuri7172gmail.com>
2024-07-10 22:26:31 +0700

    kelas jenjang untuk kode ujian

--- resources/views/validasi/blangkoijazah.blade.php
+++ resources/views/validasi/blangkoijazah.blade.php
@@ -38,9 +38,8 @@
                 }
             </style>
             <!-- Konten lainnya di sini -->
-            <img src="{{ asset('asset/images/logo_wustho.png') }}" alt="" class="absolute     opacity-25  w-1/3  p-2 mt-3 black-and-white">
-            @if ($dataKelas->first()->jenjang == 'Wustho')
 
+            @if ($dataKelas->first()->jenjang == 'Wustho')
             <img src="{{ asset('asset/images/logo_wustho.png') }}" alt="" class="absolute     opacity-25  w-1/3  p-2 mt-3 black-and-white">
             @elseif ($dataKelas->first()->jenjang == 'Ula')
             <img src="{{ asset('asset/images/logoUla.png') }}" alt="" class="absolute     opacity-25  w-1/3  p-2 mt-3 black-and-white">
