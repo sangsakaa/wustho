c7919539639bae2fbf210dc415e45e143a175357
sangsakaa <zainuri7172gmail.com>
2024-08-11 05:17:18 +0800

    pdf

--- resources/views/presensi/kelas/layoutpdf.blade.php
+++ resources/views/presensi/kelas/layoutpdf.blade.php
@@ -18,7 +18,7 @@
         td {
             border: 1px solid green;
             color: green;
-            font-size: xx-small;
+            font-size: smaller;
         }
 
         .kop_lap {
@@ -30,8 +30,9 @@
 
 
         .ket_nama {
-            width: 270px;
+            width: 280px;
             text-transform: capitalize;
+            padding-left: 2px;
         }
 
         .tulis_tengah {
@@ -53,12 +54,23 @@
     </style>
     <div>
         @if($dataKelasMi->jenjang === "Wustho")
-        <div class="font-semibold text-3xl uppercase  parent">
-            <span class=" kop_lap">MADRASAH DINIYAH {{$dataKelasMi->jenjang}} WAHIDIYAH</span> <br>
-            <span class=" kop_lap">TAHUN PELAJARAN {{$dataKelasMi->periode}} {{$dataKelasMi->ket_semester}}</span>
-            <hr class="custom-hr">
-            Laporan Harian <br>
-            Hari, tanggal : {{ $tgl->isoFormat('dddd, D MMMM YYYY') }}
+        <style>
+            .parent {
+                justify-content: center;
+                align-items: center;
+                text-align: center;
+
+                /* or specify a height if necessary */
+            }
+        </style>
+        <div class="font-semibold text-3xl uppercase  ">
+            <div class="parent ">
+                <span class=" kop_lap">MADRASAH DINIYAH {{$dataKelasMi->jenjang}} WAHIDIYAH</span> <br>
+                <span class=" kop_lap">TAHUN PELAJARAN {{$dataKelasMi->periode}} {{$dataKelasMi->ket_semester}}</span>
+                <hr class="custom-hr">
+                Laporan Harian <br>
+                Hari, tanggal : {{ $tgl->isoFormat('dddd, D MMMM YYYY') }}
+            </div>
         </div>
         @elseif($dataKelasMi->jenjang === "Ulya")
         <p class="font-semibold text-3xl uppercase">
@@ -77,52 +89,54 @@
         </p>
         @endif
     </div>
-    <table class="table-fixed  w-full text-green-900">
-        <thead class="border border-b-2 border-green-600">
-            <tr class="border border-green-600 text-xs sm:text-sm">
-                <th class="no_more border border-green-600 w-8">No</th>
-                <th class="nama_as border border-green-600 w-1/6">Asrama</th>
-                <th class="border border-green-600 w-10">Kls</th>
-                <th class="border border-green-600 w-11">Total</th>
-                <th class="border border-green-600 w-11">Tidak <br> Hadir</th>
-                <th class="border border-green-600 w-11">Hadir</th>
-                <th class="border border-green-600 w-1/3 sm:w-1/2">tidak hadir</th>
-                <th class="border border-green-600 w-10 sm:w-11">Ket</th>
-                <th class="presen border border-green-600 w-1/6">%Kehadiran</th>
-            </tr>
-        </thead>
-        <tbody class="text-sm">
-            @php
-            $nomor = 1;
-            @endphp
-            @foreach ($rekapAbsensi as $nama_asrama => $dataAsrama)
-            @foreach ($dataAsrama as $nama_kelas => $dataKelas)
-            @foreach ($dataKelas['absensi'] as $absensi)
+    <div>
+        <table class="table-fixed  w-full text-green-900">
+            <thead class="border border-b-2 border-green-600">
+                <tr class="border border-green-600 text-xs sm:text-sm">
+                    <th class="no_more border border-green-600 w-8">No</th>
+                    <th class="nama_as border border-green-600 w-1/6">Asrama</th>
+                    <th class="border border-green-600 w-10">Kls</th>
+                    <th class="border border-green-600 w-11">Total</th>
+                    <th class="border border-green-600 w-11">Tidak <br> Hadir</th>
+                    <th class="border border-green-600 w-11">Hadir</th>
+                    <th class="border border-green-600 w-1/3 sm:w-1/2">tidak hadir</th>
+                    <th class="border border-green-600 w-10 sm:w-11">Ket</th>
+                    <th class="presen border border-green-600 w-1/6">%Kehadiran</th>
+                </tr>
+            </thead>
+            <tbody class="text-sm">
+                @php
+                $nomor = 1;
+                @endphp
+                @foreach ($rekapAbsensi as $nama_asrama => $dataAsrama)
+                @foreach ($dataAsrama as $nama_kelas => $dataKelas)
+                @foreach ($dataKelas['absensi'] as $absensi)
 
-            <tr class="border border-green-600 text-xs sm:text-sm">
-                @if ($loop->first)
-                <td class="ongko tulis_tengah border border-green-600 text-center" rowspan="{{ $dataKelas['row'] }}">{{ $nomor++ }}</td>
-                @endif
-                @if ($loop->parent->first && $loop->first)
-                <td class="tulis_tengah border border-green-600 text-center text-sm" rowspan="{{ $dataAsrama->sum('row') }}">{{ $nama_asrama }}</td>
-                @endif
-                @if ($loop->first)
-                <td class="tulis_tengah border border-green-600 text-center" rowspan="{{ $dataKelas['row'] }}">{{ $nama_kelas }}</td>
-                <td class="tulis_tengah border border-green-600 text-center" rowspan="{{ $dataKelas['row'] }}">{{ $dataKelas['total'] }}</td>
-                <td class="tulis_tengah border border-green-600 text-center" rowspan="{{ $dataKelas['row'] }}">{{ $dataKelas['tidakHadir'] }}</td>
-                <td class="tulis_tengah border border-green-600 text-center" rowspan="{{ $dataKelas['row'] }}">{{ $dataKelas['hadir'] }}</td>
-                @endif
-                <td class="nama_sis ket_nama border border-green-600 text-xs capitalize">{{ $dataKelas['tidakHadir'] !== 0 ? $loop->iteration . '. ' . strtolower($absensi->nama_siswa) : 'NIHIL' }}</td>
-                <td class="tulis_tengah border border-green-600 text-center capitalize">{{ $dataKelas['tidakHadir'] !== 0 ? $absensi->keterangan : 'NIHIL' }}</td>
-                @if ($loop->first)
-                <td class="tulis_tengah border border-green-600 text-center" rowspan="{{ $dataKelas['row'] }}">{{ number_format($dataKelas['persentase'], 1, ',') }}%</td>
-                @endif
-            </tr>
-            @endforeach
-            @endforeach
-            @endforeach
-        </tbody>
-    </table>
+                <tr class="border border-green-600 text-xs sm:text-sm">
+                    @if ($loop->first)
+                    <td class="ongko tulis_tengah border border-green-600 text-center" rowspan="{{ $dataKelas['row'] }}">{{ $nomor++ }}</td>
+                    @endif
+                    @if ($loop->parent->first && $loop->first)
+                    <td class="tulis_tengah border border-green-600 text-center text-sm" rowspan="{{ $dataAsrama->sum('row') }}">{{ $nama_asrama }}</td>
+                    @endif
+                    @if ($loop->first)
+                    <td class="tulis_tengah border border-green-600 text-center" rowspan="{{ $dataKelas['row'] }}">{{ $nama_kelas }}</td>
+                    <td class="tulis_tengah border border-green-600 text-center" rowspan="{{ $dataKelas['row'] }}">{{ $dataKelas['total'] }}</td>
+                    <td class="tulis_tengah border border-green-600 text-center" rowspan="{{ $dataKelas['row'] }}">{{ $dataKelas['tidakHadir'] }}</td>
+                    <td class="tulis_tengah border border-green-600 text-center" rowspan="{{ $dataKelas['row'] }}">{{ $dataKelas['hadir'] }}</td>
+                    @endif
+                    <td class="nama_sis ket_nama border border-green-600 text-xs capitalize">{{ $dataKelas['tidakHadir'] !== 0 ? $loop->iteration . '. ' . strtolower($absensi->nama_siswa) : 'NIHIL' }}</td>
+                    <td class="tulis_tengah border border-green-600 text-center capitalize">{{ $dataKelas['tidakHadir'] !== 0 ? $absensi->keterangan : 'NIHIL' }}</td>
+                    @if ($loop->first)
+                    <td class="tulis_tengah border border-green-600 text-center" rowspan="{{ $dataKelas['row'] }}">{{ number_format($dataKelas['persentase'], 1, ',') }}%</td>
+                    @endif
+                </tr>
+                @endforeach
+                @endforeach
+                @endforeach
+            </tbody>
+        </table>
+    </div>
 </div>
 @endif
 </div>
