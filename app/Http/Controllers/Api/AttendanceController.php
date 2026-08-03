5bb20370663e144423a8ebdd33fbbb1635c1ff40
sangsaka <zainuri717@gmail.com>
2026-08-03 16:30:34 +0800

    test

--- app/Http/Controllers/Api/AttendanceController.php
+++ app/Http/Controllers/Api/AttendanceController.php
@@ -2,30 +2,32 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Http\Controllers\Controller;
 use App\Models\Absensikelas;
 use Illuminate\Http\Request;
 
-class AttendanceController
+class AttendanceController extends Controller
 {
-    //
-
-
     public function checkin(Request $request)
     {
+        $request->merge([
+            'keterangan' => ucfirst(strtolower($request->keterangan))
+        ]);
+
         $request->validate([
-            'session_id'        => 'required|exists:sesikelas,id',
-            'peserta_kelas_id'  => 'required|exists:pesertakelas,id',
-            'status'            => 'required|in:hadir,izin,sakit,alfa',
-            'alasan'            => 'nullable|string',
+            'sesikelas_id'    => 'required|exists:sesikelas,id',
+            'pesertakelas_id' => 'required|exists:pesertakelas,id',
+            'keterangan'      => 'required|in:Hadir,Izin,Sakit,Alfa',
+            'alasan'          => 'nullable|string',
         ]);
 
         $absensi = Absensikelas::updateOrCreate(
             [
-                'sesikelas_id'    => $request->session_id,
-                'pesertakelas_id' => $request->peserta_kelas_id,
+                'sesikelas_id'    => $request->sesikelas_id,
+                'pesertakelas_id' => $request->pesertakelas_id,
             ],
             [
-                'keterangan' => ucfirst(strtolower($request->status)),
+                'keterangan' => $request->keterangan,
                 'alasan'     => $request->alasan,
             ]
         );
@@ -34,8 +36,9 @@ class AttendanceController
             'success' => true,
             'message' => 'Presensi berhasil disimpan',
             'data' => [
-                'id' => $absensi->id,
-                'status' => strtolower($absensi->keterangan),
+                'id'       => $absensi->id,
+                'status'   => strtolower($absensi->keterangan),
+                'alasan'   => $absensi->alasan,
             ]
         ]);
     }
